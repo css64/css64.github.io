ff114d56b145179eaae652ee39750faaf3827eaf
Andrey <ateodorskij@gmail.com>
2021-07-30 04:59:51 +0300

    1
    1

--- js/darkmode.js
+++
@@ -1,7 +0,0 @@
-$('#switch').on('click', () => {
-    if ($('#switch').prop('checked')) {
-        $('.content').addClass('white-theme');
-    } else {
-        $('.content').removeClass('white-theme');
-    }
-})
